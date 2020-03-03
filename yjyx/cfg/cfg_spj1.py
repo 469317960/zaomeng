4b0aac059a4a4407893bb8971c4414935d45560c
“469317960 <469317960@qq.com>
2020-03-03 11:10:32 +0800

    gelete

--- yjyx/cfg/cfg_spj1.py
+++
@@ -1,3 +0,0 @@
-VCODE = '00000007191229558808'
-SCLOOL_NAME = '松勤学院00714'
-URL = 'http://ci.ytesting.com/api/3school/school_classes'
--- yjyx/tc/d-空白数据/__init__.robot
+++
@@ -1,8 +0,0 @@
-*** Settings ***
-Library   pylib.lib_class.ClassLib
-Suite Setup  delete_all_class
-
-
-
-------------------
-
